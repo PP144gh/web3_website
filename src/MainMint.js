c212d5cb391d42a6dc68176c9ff6c931ae3c6fff
pp144 <pedromanpereira@gmail.com>
2023-05-02 16:00:34 +0100

    web3 website done

---
+++ src/MainMint.js
@@ -0,0 +1,146 @@
+//import React from 'react';
+
+import React, {useState } from 'react';
+import { ethers, BigNumber} from 'ethers'; //alternative to web3.js
+import { Box, Button, Flex, Input, Text } from '@chakra-ui/react';
+
+
+import roboPunksNFT from './RoboPunksNFT.json';
+
+
+const roboPunksNFTAddress = "0x0e13Cb0A85c6e6075b6283Cfc07bEc94C9168dF9";
+
+const MainMint = ({ accounts, setAccounts}) => {
+    const [mintAmount, setMintAmount] = useState(1);
+    const isConnected = Boolean(accounts[0]);
+
+    async function handleMint() {
+
+        if (window.ethereum) {
+
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(
+                roboPunksNFTAddress,
+                roboPunksNFT.abi,
+                signer
+            );
+            try {
+                const response = await contract.mint(BigNumber.from(mintAmount), {
+                    value: ethers.utils.parseEther((0.02 * mintAmount).toString()),
+                });
+                console.log('response: ', response);
+
+            } catch(err) {
+                console.log("error: ", err)
+            }
+
+        }
+    }
+
+    const handleDecrement = () => {
+
+        if (mintAmount <= 1) return;
+        setMintAmount(mintAmount -1);
+    };
+
+    const handleIncrement = () => {
+
+        if (mintAmount >= 3) return;
+        setMintAmount(mintAmount + 1);
+    };
+
+    return (
+        <Flex justify="center" align="center" height="100vh" paddingBottom="150px">
+            <Box width="520px">
+                <div>
+            <Text fontSize="48px" textShadow="0 5px #000000">Edgy Cool Name Test</Text>
+            <Text
+                fontSize="30px"
+                letterSpacing="-5.5%"
+                fontFamily="VT323"
+                textShadow="0 2px 2px #000000"
+            
+            
+            
+            >
+                Test Text. Descriptive
+                </Text>
+            </div>
+
+            {isConnected ? (
+                <div>
+                    <Flex align="center" justify="center">
+                        <Button
+                         backgroundColor="#0096FF"
+                         borderRadius="5px"
+                         boxShadow="0px 2px 2px 1px #0F0F0F"
+                         color="white"
+                         cursor="pointer"
+                         fontFamily="inherit"
+                         padding="15px"
+                         marginTop="10px"
+                         onClick={handleDecrement}
+                         >
+                            -
+                        </Button>
+                        <Input
+                         readOnly
+                         fontFamily="inherit"
+                         width="100px"
+                         height="40px"
+                         textAlign="center"
+                         paddingLeft="19px"
+                         marginTop="10px" 
+                        type="number" 
+                        value={mintAmount}
+                         />
+                        <Button
+                         backgroundColor="#0096FF"
+                         borderRadius="5px"
+                         boxShadow="0px 2px 2px 1px #0F0F0F"
+                         color="white"
+                         cursor="pointer"
+                         fontFamily="inherit"
+                         padding="15px"
+                         marginTop="10px"
+                        onClick={handleIncrement}
+                        >
+                            +
+                        </Button>
+                    </Flex>
+                    <Button
+                         backgroundColor="#0096FF"
+                         borderRadius="5px"
+                         boxShadow="0px 2px 2px 1px #0F0F0F"
+                         color="white"
+                         cursor="pointer"
+                         fontFamily="inherit"
+                         padding="15px"
+                         marginTop="10px"
+                         onClick={handleMint}
+                         >
+                            MINT NOW
+                    </Button>
+                    </div>
+            ) : (
+                <Text
+                marginTop="70px"
+                fontSize="30px"
+                letterSpacing="-5.5%"
+                fontFamily="VT323"
+                textShadow="0 3px #000000"
+                color="#FFFF00" 
+            >
+                CONNECT TO MINT.
+                </Text>
+            )}
+            </Box>
+        </Flex>
+    );
+
+};
+
+export default MainMint;
+
+
